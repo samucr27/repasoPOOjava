b24288742a5de815738e3a9b9eef2cf89bee17f9
Samuel Castellanos Rivera <castellanosriverasamuel@gmail.com>
2026-02-10 13:31:20 -0500

    tercer cambio

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,21 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hola mundo");
-        System.out.println("Hello other world");
-        System.out.println("bai");
+        //crear persona desde el constructor vacio
+        Persona p1 = new Persona();
 
+        p1.setNombre("Juan");
+        p1.setEdad(26);
+        p1.setGenero("Masculino");
+
+        //crear persona usando construcor con todos los parametros
+
+        Persona p2 = new Persona("Juliana",23,"Femenino");
+
+        //mostrar persona
+
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
 
         }
 }
---
+++ src/main/java/org/example/Persona.java
@@ -0,0 +1,67 @@
+package org.example;
+
+public class Persona {
+
+
+    //atributos
+    private String nombre;
+    private int edad;
+    private String genero;
+
+
+    //constructores
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, int edad, String genero) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.genero = genero;
+    }
+
+    //getter-setter
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    //to string
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", genero='" + genero + '\'' +
+                '}';
+    }
+
+    public int calcularPasos(){
+        return 0;
+    }
+    public double calcularSalario(){
+        return 0.0;
+    }
+}
